b936371a561d5d0ef3011b91f1a25846d351f623
Muhammad Issa <123868209+issabaloach@users.noreply.github.com>
2024-03-18 19:56:09 +0500

    updated array exercise

--- Chap 14-17/script.js
+++ Chap 14-17/script.js
@@ -196,4 +196,86 @@ document.write(" <br> joined array = "  , joinedanimal)
 // Create a new array. Store values one by one in such a way
 // that you can access the values in the order in which they were
 // stored. (FIFO-First In First Out)
+// Create an empty array
+
+document.write("<br>")
+document.write("<h2> Question 17 is in Console FIFO </h2>")
+var fifo = ["keyboard" , "mouse", "trackpad", "touchpad" , "monitor"]
+console.log(fifo)
+console.log("out " ,fifo.shift(0));
+console.log("out " ,fifo.shift(1));
+console.log("out " ,fifo.shift(2));
+console.log("out " ,fifo.shift(3));
+console.log("out " ,fifo.shift(4));
+
+
+// Question 18
+// Create a new array. Store values one by one in such a way
+// that you can access the values in reverse order. (Last In-First
+// Out)
+
+document.write("<br>")
+document.write("<h2> Question 18 is in Console LIFO </h2>")
+var lifo = ["motorola" , "iphone", "google pixel", "nothing" , "samsung"]
+console.log(lifo)
+console.log("out " ,lifo.pop(0));
+console.log("out " ,lifo.pop(1));
+console.log("out " ,lifo.pop(2));
+console.log("out " ,lifo.pop(3));
+console.log("out " ,lifo.pop(4));
+
+// QUESTION 19
+// Write a program to store phone manufacturers (Apple,
+  // Samsung, Motorola, Nokia, Sony & Haier) in an array. Display
+  // the following dropdown/select menu in your browser using
+  // document.write() method:
+
+   document.write("<br>");
+   document.write("<h3> Dropdown </h3>")
+    const manufacturers = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+
+    function createDropdown() {
+       
+        const select = document.createElement("select");
+
+ 
+        const defaultOption = document.createElement("option");
+        defaultOption.text = "Select a Manufacturer";
+        defaultOption.value = "";
+        select.add(defaultOption);
+
+        for (const manufacturer of manufacturers) {
+            const option = document.createElement("option");
+            option.text = manufacturer;
+            option.value = manufacturer;
+            select.add(option);
+        }
+        document.write(select.outerHTML);
+    }
+
+    createDropdown()
+  
+    // QUESTION 20
+    // Declare and initialize an empty multidimensional array.
+    //    (Array of arrays)
+     
+    var multidimensional_array=[
+      [],
+      [],
+      []
+    ]
+
+    console.log(multidimensional_array);
+
+    // QUESTION 21
+    // Declare and initialize a multidimensional array representing
+// the following matrix
+
+var matrix = [
+  [1, 2, 3],  
+  [4, 5, 6],  
+  [7, 8, 9]   
+];
+document.write(matrix)
+console.log(matrix[1][2]); 
 
